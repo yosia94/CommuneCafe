18eaf3c43d57667a3bf1d018f03ffc09699c671a
Yosia Andre <yosiandrex@gmail.com>
2026-07-21 21:45:07 +0700

    minor update

--- server.js
+++ server.js
@@ -262,6 +262,14 @@ async function sendEmail(to, subject, text) {
   }
 }
 
+// Authentication middleware
+function isAuthenticated(req, res, next) {
+  if (req.session.admin) {
+    return next();
+  }
+  res.redirect('/admin');
+}
+
 // -------------------- ADMIN CMS --------------------
 
 // Admin login page
